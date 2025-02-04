23f9e2a9b32711c9d59a9cc1f5d01d199eaba22e
1y4nu <akanoim@gmail.com>
2025-02-03 21:42:24 -0400

    Added Bot Responses

--- responses.py
+++ responses.py
@@ -1,2 +1,18 @@
-def get_response(user_in: str) -> str:
-    raise NotImplementedError('Code is missing....')
+from random import choice, randint
+
+
+def get_response(user_input: str) -> str:
+    lowered: str = user_input.lower()
+
+    if lowered == '':
+        return 'Your really silent......'
+    elif 'hello' in lowered:
+        return 'Hello there!'
+    elif 'bye' in lowered:
+        return 'See you later!'
+    elif 'roll dice' in lowered:
+        return f'You rolled: {randint(1, 6)}'
+    else:
+        return choice(['I do not understand...',
+                       'What did you say?',
+                       'Can you reprhase that?'])
