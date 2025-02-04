b9e905d943f7474e482db83ec5a257c704aa7ba2
1y4nu <akanoim@gmail.com>
2025-02-03 21:50:13 -0400

    Added Message Functionality and Handling of messages

--- main.py
+++ main.py
@@ -8,3 +8,48 @@ from responses import get_response
 # Load in token
 load_dotenv()
 TOKEN : Final[str] = os.getenv('DISCORD_TOKEN')
+
+# Bot Setup
+intents : Intents = Intents.default()
+intents.message_content = True
+client : Client = Client(intents=intents)
+
+# Message Functionality
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('(Message was empty because intents were not enabled most likely)')
+        return
+
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+
+    try:
+        response : str = get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+# Handling startup
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running')
+
+# Handling incoming messages
+@client.event
+async def on_message(message : Message) -> None:
+    if message.author == client.user:
+        return
+
+    username : str = str(message.author)
+    user_message : str = message.content
+    channel : str = str(message.channel)
+
+    print(f'[{channel}] {username} : "{user_message}"')
+    await send_message(message, user_message)
+
+# Main Entry
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
